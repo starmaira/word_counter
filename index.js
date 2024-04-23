792b49bafe6ac8a0399b618c086c12feb3f4df4a
Maira_Nasir <missmaira924@gmail.com>
2024-04-23 21:59:20 +0500

    push my word counter programm in githun

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/en node    //shebang operating system to run it with node.js///
+//inquirer is imporeted, which is ca command line interface for node.js//
+import inquirer from "inquirer";
+//Declare a contant 'answer' and assign it to the result of inquirer.prompt function//
+const answer = await inquirer.prompt([
+    {
+        name: "sentence", //property //
+        type: "input",
+        message: "Enter a sentence to count the word in it: "
+    }
+]);
+const words = answer.sentence.trim().split(" "); //  .  notation methode // trim removes spaces only from start and at the end // split convert alphabets into proper words, and ( ) bracket have space which splits words to word and make them countable //
+//print the array of words to the console.log
+console.log(words);
+console.log(`The number of words in the sentence are ${words.length}`);
